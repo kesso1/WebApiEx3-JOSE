37f9b809be4222d42a11ffaa03ed2eaee3980725
otti <othmar.kesseli@outlook.com>
2018-03-11 14:35:33 +0100

    Add project files.

---
+++ WebApiEx3-JOSE/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApiEx3_JOSE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
